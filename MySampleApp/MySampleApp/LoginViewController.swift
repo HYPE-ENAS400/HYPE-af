9e19a0be1914cddba3d0ace23fcb65e8062169a3
Max Payson <maxwell.payson@yale.edu>
2016-04-08 13:02:56 -0400

    added loginVC UI

---
+++ MySampleApp/MySampleApp/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  Hype-2
+//
+//  Created by max payson on 4/8/16.
+//  Copyright © 2016 Amazon. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+
+    @IBOutlet var userNameTextEdit: UITextField!
+    @IBOutlet var passwordTextEdit: UITextField!
+    @IBOutlet var signUpButton: UIButton!
+    @IBOutlet var logInButton: UIButton!
+    
+    
+}
