6ee05be96276342fd815719f16f25d7b164c9d05
Max Payson <maxwell.payson@yale.edu>
2016-04-10 13:47:07 -0400

    installed firebase coco

--- MySampleApp/Pods/Target Support Files/Pods-Hype-2/Pods-Hype-2-frameworks.sh
+++ MySampleApp/Pods/Target Support Files/Pods-Hype-2/Pods-Hype-2-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Hype-2/Firebase.framework"
   install_framework "Pods-Hype-2/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Hype-2/Firebase.framework"
   install_framework "Pods-Hype-2/pop.framework"
 fi
